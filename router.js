1386306ea2b0af980ea56704ef78ee01d0bb3f29
yeokyeong <yeokyeong46@gmail.com>
2018-11-29 16:39:56 +0900

    yk ver1 addition

--- router.js
+++ router.js
@@ -3,11 +3,15 @@ const express = require('express');
 const router = express.Router();
 
 const index = require('./routers/index');
-//const users = require('./routers/users');
-//const img = require('./routers/img');
+
+const manager_page_list = require('./routers/manager_page_list');
+const get_client_profile =require('./routers/get_client_profile');
+const get_freelancer_profile =require('./routers/get_freelancer_profile');
 
 router.use('/', index);
-//router.use('/users', users);
-//router.use('/img', img);
+
+router.use('/manager_page_list', manager_page_list);
+router.use('/get_client_profile', get_client_profile);
+router.use('/get_freelancer_profile', get_freelancer_profile);
 
 module.exports = router;
--- routers/index.js
+++ routers/index.js
@@ -28,7 +28,17 @@ router.get('/signin_freelancer', (req, res, next) => {
   res.type('html').sendFile(path.join(__dirname, '../public/html/signin_freelancer.html'));
 });
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+router.get('/manager_page', (req, res, next) => {
+  res.type('html').sendFile(path.join(__dirname, '../public/html/manager_page.html'));
+});
 
+router.get('/client_profile', (req, res, next) => {
+  res.type('html').sendFile(path.join(__dirname, '../public/html/client_profile.html'));
+});
+
+router.get('/freelancer_profile', (req, res, next) => {
+  res.type('html').sendFile(path.join(__dirname, '../public/html/freelancer_profile.html'));
+});
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 module.exports = router;
