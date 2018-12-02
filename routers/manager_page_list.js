d7f484de1f8a149b8a40af757c402ccbf93362c0
yeokyeong <yeokyeong46@gmail.com>
2018-12-02 23:32:05 +0900

    쿼리들 대충 다 수정 된 듯

--- routers/manager_page_list.js
+++ routers/manager_page_list.js
@@ -6,34 +6,26 @@ const db = require('../modules/db');
 
 router.get('/get_client_list', wrapper.asyncMiddleware(async (req, res, next) => {
   const user = await db.getQueryResult('SELECT Id, Name, Phone, Score FROM client');
-  //console.log('-------------------------------');
-  //console.log(JSON.stringify(user, null, 2));
-	//console.log('-------------------------------');
   res.json(user);
 }));
 
 router.get('/get_freelancer_list', wrapper.asyncMiddleware(async (req, res, next) => {
     const user = await db.getQueryResult('SELECT Id, Name, Age, Career, Major, Phone, Score FROM freelancer');
-    //console.log('-------------------------------');
-    //console.log(JSON.stringify(user, null, 2));
-    //console.log('-------------------------------');
     res.json(user);
   }));
 
 router.post('/delete_freelancer', wrapper.asyncMiddleware(async (req, res, next) =>{
     const Id = req.body.id;
-    console.log(Id);
-    var ret = await db.getQueryResult("DELETE FROM freelancer WHERE Id='"+Id+"'");
+    var ret = await db.getQueryResult("DELETE FROM FREELANCER WHERE ID='"+Id+"' AND ID NOT IN (SELECT DISTINCT Participant_id FROM WORK WHERE State!='COMPLETED')");
     console.log(ret);
-    res.json({success: true});
+    res.json(ret);
 }));
 
 router.post('/delete_client', wrapper.asyncMiddleware(async (req, res, next) =>{
   const Id = req.body.id;
-  console.log(Id);
-  var ret = await db.getQueryResult("DELETE FROM client WHERE Id='"+Id+"'");
+  var ret = await db.getQueryResult("DELETE FROM CLIENT WHERE Id NOT IN (SELECT DISTINCT Client_id FROM REQUEST WHERE STATE='WORKING') AND Id='"+Id+"'");
   console.log(ret);
-  res.json({success: true});
+  res.json(ret);
 }));
 
 module.exports = router;
