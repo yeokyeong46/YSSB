759acde94606e1e7d28d6cf4cfd642f3da77e047
Unknown <gyurinida@naver.com>
2018-12-02 14:59:26 +0900

    config error

--- config.js
+++ config.js
@@ -1,20 +1,21 @@
-port: 3000,
-appRoot: __dirname,
-db: {
-  connectionLimit: 20,  // 크게 고려 안해도 됨
-  host: 'localhost',
-  user: 'root',
-port: 3306,
-  password: '',
-  database: 'no_team',
-},
-session_config: {
-  secret: 'FEjfsdklewN@$(&*(F))&',
-  resave: false,
-  saveUninitialized: true
-},
-admin_config: {
-  id: 'admin',
-  pwd: 'admin'
-}
+module.exports={
+  port: 3000,
+  appRoot: __dirname,
+  db: {
+    connectionLimit: 20,  // 크게 고려 안해도 됨
+    host: 'localhost',
+    user: 'root',
+  port: 3306,
+    password: '',
+    database: 'no_team',
+  },
+  session_config: {
+    secret: 'FEjfsdklewN@$(&*(F))&',
+    resave: false,
+    saveUninitialized: true
+  },
+  admin_config: {
+    id: 'admin',
+    pwd: 'admin'
+  }
 };
