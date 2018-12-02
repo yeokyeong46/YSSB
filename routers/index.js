c67b941b6007d1c07580e4795499067e337ec75d
Unknown <gyurinida@naver.com>
2018-12-02 13:08:42 +0900

    login, logout 구현

--- routers/index.js
+++ routers/index.js
@@ -2,26 +2,67 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const wrapper = require('../modules/wrapper');
+const db = require('../modules/db');
+const config = require('../config');
 
 router.get('/', (req, res, next) => {
   res.render('index', {
-    sess_level: -1,
-    sess_id: -1
+    sess_level: req.session.auth_level,
+    sess_id: req.session.curr_id
   });
-  //res.type('html').sendFile(path.join(__dirname, '../public/html/index.html'));
 });
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 router.get('/index', (req, res, next) => {
   res.render('index', {
-    sess_level: -1,
-    sess_id: -1
+    sess_level: req.session.auth_level,
+    sess_id: req.session.curr_id
   });
-  //res.type('html').sendFile(path.join(__dirname, '../public/html/index.html'));
 });
 
 router.get('/login', (req, res, next) => {
-  res.type('html').sendFile(path.join(__dirname, '../public/html/login.html'));
+  res.render('login', {
+    sess_level: -1,
+    sess_id: -1
+  });
+});
+
+router.post('/login', wrapper.asyncMiddleware(async (req, res, next) => {
+  var id = req.body.login_id;
+  var pwd = req.body.login_pwd;
+
+  var queryC = "SELECT * FROM CLIENT WHERE Id='"+id+"'and Password='"+pwd+"';";
+  var queryF = "SELECT * FROM FREELANCER WHERE Id='"+id+"'and Password='"+pwd+"';";
+
+  var resultC = await db.getQueryResult(queryC);
+  var resultF = await db.getQueryResult(queryF);
+
+  var level=-1; // -1:not login, 0:admin, 1:client, 2:freelancer
+  if(id==config.admin_config.id && pwd==config.admin_config.pwd){
+    level = 0;
+  }
+  else if(Object.keys(resultC).length > 0){
+    level = 1;
+  }
+  else if(Object.keys(resultF).length > 0){
+    level = 2;
+  }
+
+  if(level!=-1){
+    req.session.auth_level = level;
+    req.session.curr_id = id;  
+    res.redirect('/');
+  }
+  else{
+    var msg = '<script type="text/javascript">alert("해당하는 회원정보가 존재하지 않습니다.");window.location.href="/login"</script>';
+    res.send(msg);
+  }
+}));
+
+router.get('/logout', (req, res, next) => {
+  req.session.destroy();
+  res.redirect('/login');
 });
 
 router.get('/signup', (req, res, next) => {
