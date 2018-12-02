7813a31ab69e0820dd037e93623fce6972d329cd
chaeyeon <bravoyourlif@naver.com>
2018-12-02 14:49:14 +0900

    get_view_message

--- routers/get_view_message.js
+++ routers/get_view_message.js
@@ -4,9 +4,8 @@ const router = express.Router();
 const wrapper = require('../modules/wrapper');
 const db = require('../modules/db');
 
-router.get('/view_message_info/:request_id/:participant_id', wrapper.asyncMiddleware(async (req, res, next) => {
+router.get('/view_message_info/:request_id', wrapper.asyncMiddleware(async (req, res, next) => {
     var Rid = req.params.request_id;
-    var Pid = req.params.participant_id;
     var user = await db.getQueryResult("SELECT Request_id, Participant_id FROM message WHERE Request_id='"+Rid+"'");
     //console.log('-------------------------------');
     //console.log(JSON.stringify(user, null, 2));
@@ -14,9 +13,8 @@ router.get('/view_message_info/:request_id/:participant_id', wrapper.asyncMiddle
     res.json(user);
 }));
 
-router.get('/view_message/:request_id/:participant_id', wrapper.asyncMiddleware(async (req, res, next) => {
+router.get('/view_message/:request_id', wrapper.asyncMiddleware(async (req, res, next) => {
     var Rid = req.params.request_id;
-    var Pid = req.params.participant_id;
     var user = await db.getQueryResult("SELECT Id, Contents FROM message WHERE Request_id='"+Rid+"'");
     //console.log('-------------------------------');
     //console.log(JSON.stringify(user, null, 2));
