5d1597649c31394d55d9d6e402f798e5e6a26211
chaeyeon <bravoyourlif@naver.com>
2018-12-02 14:56:56 +0900

    config 다시 변경

--- config.js
+++ config.js
@@ -1,12 +1,20 @@
-module.exports = {
-  port: 3000,
-  appRoot: __dirname,
-  db: {
-    connectionLimit: 20,  // 크게 고려 안해도 됨
-    host: 'localhost',
-    user: 'root',
-	port: 3306,
-    password: 'Wywl0515!!',
-    database: 'no_team',
-  },
+port: 3000,
+appRoot: __dirname,
+db: {
+  connectionLimit: 20,  // 크게 고려 안해도 됨
+  host: 'localhost',
+  user: 'root',
+port: 3306,
+  password: '',
+  database: 'no_team',
+},
+session_config: {
+  secret: 'FEjfsdklewN@$(&*(F))&',
+  resave: false,
+  saveUninitialized: true
+},
+admin_config: {
+  id: 'admin',
+  pwd: 'admin'
+}
 };
