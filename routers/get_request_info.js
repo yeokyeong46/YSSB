90e97ebb3360f7013d9a714f53bb406fa2f6f5f0
chaeyeon <bravoyourlif@naver.com>
2018-12-05 02:24:41 +0900

    의뢰자 프리랜서 선택/거절 기능 추가

--- routers/get_request_info.js
+++ routers/get_request_info.js
@@ -6,7 +6,8 @@ const db = require('../modules/db');
 
 router.get('/:Id', wrapper.asyncMiddleware(async (req, res, next) => {
     var Id = req.params.Id;
-    var user = await db.getQueryResult("SELECT Id, Title, Pay, Min_career, State, Client_id FROM request WHERE Id='"+Id+"'");
+    console.log("hi");
+    var user = await db.getQueryResult("SELECT Id, Title, Pay, Min_career, State, Client_id FROM request WHERE Id="+Id);
     //console.log('-------------------------------');
     //console.log(JSON.stringify(user, null, 2));
         //console.log('-------------------------------');
@@ -22,7 +23,7 @@ router.post('/delete_request', wrapper.asyncMiddleware(async (req, res, next) =>
 
 router.get('/get_worker_list/:Id', wrapper.asyncMiddleware(async (req, res, next) => {
     var Id = req.params.Id;
-    var user = await db.getQueryResult("SELECT Participant_id, State FROM work WHERE Request_id='"+Id+"'");
+    var user = await db.getQueryResult("SELECT Participant_id, State FROM work WHERE Request_id="+Id);
     //console.log('-------------------------------');
     //console.log(JSON.stringify(user, null, 2));
         //console.log('-------------------------------');
@@ -31,11 +32,32 @@ router.get('/get_worker_list/:Id', wrapper.asyncMiddleware(async (req, res, next
 
 router.get('/get_applier_list/:Id', wrapper.asyncMiddleware(async (req, res, next) => {
     var Id = req.params.Id;
-    var user = await db.getQueryResult("SELECT Participant_id, State FROM apply WHERE Request_id='"+Id+"'");
+    var user = await db.getQueryResult("SELECT A.Participant_id AS Pid, A.State AS State, R.Client_id AS Cid FROM apply A, request R WHERE A.Request_id="+Id+" AND A.State='WAITING' AND R.Id="+Id);
     //console.log('-------------------------------');
     //console.log(JSON.stringify(user, null, 2));
         //console.log('-------------------------------');
     res.json(user);
+}));
+
+router.post('/accept_request', wrapper.asyncMiddleware(async (req, res, next) =>{
+    const Request_id = req.body.Request_id;
+    const Participant_id = req.body.Participant_id;
+    const Apply_state = req.body.Apply_state;
+    const Work_state = req.body.Work_state;
+    var ret = await db.getQueryResult("UPDATE APPLY SET State='"+Apply_state+"' WHERE Request_id="+Request_id+" AND Participant_id='"+Participant_id+"'");
+    var ret2 = await db.getQueryResult("INSERT into WORK (Request_id, Participant_id, State) VALUES ('"+Request_id+"', '"+Participant_id+"', '"+Work_state+"')");
+    console.log(ret);
+    console.log(ret2);
+    res.json({success: true});
+}));
+
+router.post('/reject_request', wrapper.asyncMiddleware(async (req, res, next) =>{
+  const Request_id = req.body.Request_id;
+  const Participant_id = req.body.Participant_id;
+  const Apply_state = req.body.Apply_state;
+  var ret = await db.getQueryResult("UPDATE APPLY SET State='"+Apply_state+"' WHERE Request_id="+Request_id+" AND Participant_id='"+Participant_id+"'");
+  console.log(ret);
+    res.json({success: true});
 }));
 
 router.post('/apply', wrapper.asyncMiddleware(async (req, res, next) =>{
