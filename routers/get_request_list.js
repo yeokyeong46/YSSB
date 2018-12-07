fdf542b1b66eb5acebc3deff7ac8b0a06e9300ad
chaeyeon <bravoyourlif@naver.com>
2018-12-07 16:28:56 +0900

    의뢰신청 버튼

--- routers/get_request_list.js
+++ routers/get_request_list.js
@@ -5,16 +5,16 @@ const wrapper = require('../modules/wrapper');
 const db = require('../modules/db');
 
 router.get('/', wrapper.asyncMiddleware(async (req, res, next) => {
-  var user = await db.getQueryResult("SELECT Id, Title, Pay, DATE_FORMAT(Working_start_date,'%Y-%m-%d') Working_start_date, Min_career, State FROM request");
+  var user = await db.getQueryResult("SELECT Id, Title, Pay, DATE_FORMAT(Apply_start_date,'%Y-%m-%d') Working_start_date, Min_career, State FROM request");
   //console.log('-------------------------------');
   //console.log(JSON.stringify(user, null, 2));
 	//console.log('-------------------------------');
   res.json(user);
-  //console.log(sess_id);
+  console.log(sess_id);
 }));
 
 router.get('/get_offering_request', wrapper.asyncMiddleware(async (req, res, next) => {
-  var user = await db.getQueryResult("SELECT Id, Title, Pay, DATE_FORMAT(Working_start_date,'%Y-%m-%d') Working_start_date, Min_career, State FROM request WHERE State='APPLIABLE'");
+  var user = await db.getQueryResult("SELECT Id, Title, Pay, DATE_FORMAT(Apply_start_date,'%Y-%m-%d') Apply_start_date, Min_career, State FROM request WHERE State='APPLIABLE'");
   //console.log('-------------------------------');
   //console.log(JSON.stringify(user, null, 2));
 	//console.log('-------------------------------');
@@ -22,7 +22,7 @@ router.get('/get_offering_request', wrapper.asyncMiddleware(async (req, res, nex
 }));
 
 router.get('/get_offering_early_request', wrapper.asyncMiddleware(async (req, res, next) => {
-  var user = await db.getQueryResult("SELECT Id, Title, Pay, DATE_FORMAT(Working_start_date,'%Y-%m-%d') Working_start_date, Min_career, State FROM request WHERE State='APPLIABLE' ORDER BY Working_start_date");
+  var user = await db.getQueryResult("SELECT Id, Title, Pay, DATE_FORMAT(Apply_start_date,'%Y-%m-%d') Apply_start_date, Min_career, State FROM request WHERE State='APPLIABLE' ORDER BY Apply_start_date");
   //console.log('-------------------------------');
   //console.log(JSON.stringify(user, null, 2));
 	//console.log('-------------------------------');
@@ -30,7 +30,7 @@ router.get('/get_offering_early_request', wrapper.asyncMiddleware(async (req, re
 }));
 
 router.get('/get_offering_pricy_request', wrapper.asyncMiddleware(async (req, res, next) => {
-  var user = await db.getQueryResult("SELECT Id, Title, Pay, DATE_FORMAT(Working_start_date,'%Y-%m-%d') Working_start_date, Min_career, State FROM request WHERE State='APPLIABLE' ORDER BY Pay");
+  var user = await db.getQueryResult("SELECT Id, Title, Pay, DATE_FORMAT(Apply_start_date,'%Y-%m-%d') Apply_start_date, Min_career, State FROM request WHERE State='APPLIABLE' ORDER BY Pay");
   //console.log('-------------------------------');
   //console.log(JSON.stringify(user, null, 2));
 	//console.log('-------------------------------');
