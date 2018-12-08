5812b0cb2ac9f3ec39a2fa84349c197e6cb9a89b
Unknown <gyurinida@naver.com>
2018-12-08 17:53:44 +0900

    용어 수정 및 전체의뢰 목록 쿼리 수정
    의뢰신청 시작 날짜 (= 의뢰등록 날짜), (의뢰신청마감날짜), 개발시작날짜, 개발종료날짜

--- routers/get_request_list.js
+++ routers/get_request_list.js
@@ -5,7 +5,7 @@ const wrapper = require('../modules/wrapper');
 const db = require('../modules/db');
 
 router.get('/', wrapper.asyncMiddleware(async (req, res, next) => {
-  var user = await db.getQueryResult("SELECT Id, Title, Pay, DATE_FORMAT(Apply_start_date,'%Y-%m-%d') Working_start_date, Min_career, State FROM request");
+  var user = await db.getQueryResult("SELECT Id, Title, Pay, DATE_FORMAT(Apply_start_date,'%Y-%m-%d') Apply_start_date, DATE_FORMAT(Working_start_date,'%Y-%m-%d') Working_start_date, Min_career, State FROM request;");
   //console.log('-------------------------------');
   //console.log(JSON.stringify(user, null, 2));
 	//console.log('-------------------------------');
