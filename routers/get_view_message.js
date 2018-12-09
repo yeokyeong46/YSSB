43af48c5ee2920bec802ba0edfe34fca5683e808
chaeyeon <bravoyourlif@naver.com>
2018-12-09 17:43:27 +0900

    message sender id

--- routers/get_view_message.js
+++ routers/get_view_message.js
@@ -7,16 +7,14 @@ const db = require('../modules/db');
 router.post('/send_message', wrapper.asyncMiddleware(async (req, res, next) =>{
     const Rid = req.body.id;
     const Value = req.body.value;
+    const Sender_id = req.body.sender_id;
     console.log(Rid);
     console.log(Value);
     var queryM1 = "SELECT * FROM message WHERE Request_id ="+Rid+";"
     var resultM1 = await db.getQueryResult(queryM1);
     var num_message = Object.keys(resultM1).length+1;
-    var queryM2 = "SELECT Participant_id FROM work WHERE Request_id ="+Rid+";"
-    var resultM2 = await db.getQueryResult(queryM2);
-    const Pid = resultM2[0]['Participant_id'];
 
-    var ret = await db.getQueryResult("INSERT INTO message (Id, Request_id, Participant_id, Contents) VALUES ('"+num_message+"', '"+Rid+"', '"+Pid+"', '"+Value+"')");
+    var ret = await db.getQueryResult("INSERT INTO message (Id, Request_id, Sender_id, Contents) VALUES ('"+num_message+"', '"+Rid+"', '"+Sender_id+"', '"+Value+"')");
     console.log(ret);
     res.json({success: true});
 }));
